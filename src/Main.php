0f33fd2d4fd2f18a6d74f5056b9b53bb09d69ecb
Jack Honour <JaxkDev@gmail.com>
2022-08-09 16:03:00 +0100

    Prerequisite/dependency checks added.

--- src/Main.php
+++ src/Main.php
@@ -12,8 +12,62 @@
 
 namespace JaxkDev\DiscordAccount;
 
+use JaxkDev\DiscordBot\Plugin\Main as DiscordBot;
+use Phar;
+use pocketmine\plugin\DisablePluginException;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\VersionString;
+use poggit\libasynql\libasynql;
 
 class Main extends PluginBase{
 
+    private DiscordBot $discord;
+
+    public function onLoad(): void{
+        $this->checkPrerequisites();
+    }
+
+    public function onEnable(): void{
+        if($this->getServer()->getPluginManager()->getPlugin("DiscordBot")?->isEnabled() !== true){
+            $this->disable("DiscordBot is not enabled! Dependency must be enabled for this plugin to operate.");
+        }
+    }
+
+    private function checkPrerequisites(): void{
+        //Phar
+        if(Phar::running() === ""){
+            $this->disable("Plugin running from source, please use a release phar.");
+        }
+
+        //Virions
+        if(!class_exists(libasynql::class)){
+            $this->disable("Missing libasynql virion, please use a release phar.");
+        }
+
+        //Dependencies
+        $discordBot = $this->getServer()->getPluginManager()->getPlugin("DiscordBot");
+        if($discordBot === null){
+            return; //Will never happen.
+        }
+        if($discordBot->getDescription()->getWebsite() !== "https://github.com/DiscordBot-PMMP/DiscordBot"){
+            $this->disable("Incompatible dependency 'DiscordBot' detected, see https://github.com/DiscordBot-PMMP/DiscordBot/releases for the correct plugin.");
+        }
+        $ver = new VersionString($discordBot->getDescription()->getVersion());
+        if($ver->getMajor() !== 2){
+            $this->disable("Incompatible dependency 'DiscordBot' detected, v2.x.y is required however v{$ver->getBaseVersion()}) is installed, see https://github.com/DiscordBot-PMMP/DiscordBot/releases for downloads.");
+        }
+        if(!$discordBot instanceof DiscordBot){
+            $this->disable("Incompatible dependency 'DiscordBot' detected.");
+        }
+        $this->discord = $discordBot;
+    }
+
+    /**
+     * @throw DisablePluginException
+     * @return never-returns
+     */
+    private function disable(string $message): void{
+        $this->getLogger()->critical($message);
+        throw new DisablePluginException($message); //message isn't always shown to user so send critical message.
+    }
 }
