1f791cf6f329e0d638e1ac0fbe1b1d862bdfdf2f
Jack Honour <JaxkDev@gmail.com>
2022-08-09 15:18:09 +0100

    Skeleton plugin base. - Composer - PHPStan - Plugin manifest

---
+++ src/Main.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * DiscordAccount, PocketMine-MP Plugin.
+ *
+ * Licensed under the Open Software License version 3.0 (OSL-3.0)
+ * Copyright (C) 2022-present JaxkDev
+ *
+ * Twitter :: @JaxkDev
+ * Discord :: JaxkDev#2698
+ * Email   :: JaxkDev@gmail.com
+ */
+
+namespace JaxkDev\DiscordAccount;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+}
