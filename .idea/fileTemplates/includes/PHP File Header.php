fb509b93b7f330c4a8932d90b09df7a345e81185
Jack Honour <JaxkDev@gmail.com>
2023-09-03 22:10:34 +0100

    gitignore.

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,9 @@
+/*
+ * DiscordAccount, PocketMine-MP Plugin.
+ *
+ * Licensed under the Open Software License version 3.0 (OSL-3.0)
+ * Copyright (C) 2022-present JaxkDev
+ *
+ * Discord :: JaxkDev
+ * Email   :: JaxkDev@gmail.com
+ */
